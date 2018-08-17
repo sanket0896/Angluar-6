40bac2097cf7c33ae0befce496efcd8aba9cc7e8
6adityag8 <adityag@mindfiresolutions.com>
2018-08-17 13:30:56 +0530

    change the title

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Hello World, its my first Angular 6 code';
 }
