55786590040e2fd25cca942ec290ce0847e98c04
6adityag8 <adityag@mindfiresolutions.com>
2018-08-17 14:46:47 +0530

    Interpolation

--- binding/src/app/learning/learning.component.ts
+++ binding/src/app/learning/learning.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearningComponent implements OnInit {
 
+  public myname = "Kabira"
   constructor() { }
 
   ngOnInit() {
   }
-
+  public func() {
+    return "Called from function.";
+  }
 }
