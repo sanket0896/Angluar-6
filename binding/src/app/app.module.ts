410d669cc85f338d3e9b5109dae99d828f2a3046
6adityag8 <adityag@mindfiresolutions.com>
2018-08-17 15:27:00 +0530

    add FormsModule in app.module

--- binding/src/app/app.module.ts
+++ binding/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LearningComponent } from './learning/learning.component';
@@ -10,7 +11,8 @@ import { LearningComponent } from './learning/learning.component';
     LearningComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
